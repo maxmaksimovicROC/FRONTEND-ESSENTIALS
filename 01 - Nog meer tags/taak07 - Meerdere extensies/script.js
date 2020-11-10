fca0ec6084034271f113cad69da57b1a4b943516
maxmaksimovic <maxmaksimovicroc@gmail.com>
2020-11-10 18:18:51 +0100

    al mijn opdrahten

---
+++ 01 - Nog meer tags/taak07 - Meerdere extensies/script.js
@@ -0,0 +1,1 @@
+alert("de links werken")
---
+++ opdracht/eindopdracht/script.js
@@ -0,0 +1,54 @@
+let name = prompt("Wat is je naam")
+
+let email = prompt("Vul je email in")
+
+let age = prompt("Hoe oud ben je")
+
+let woonplaats = prompt("Vul je woon plaats in")
+
+
+
+if(age == 16)
+    {
+        document.body.style.background = "#ff9900"
+    }
+
+if(age == 17)
+    {
+        document.body.style.background = "#0000FF"
+    }
+if(age == 18)
+    {
+        document.body.style.background = "#ffcc00"
+    }
+
+
+if(age == 19)
+    {
+        document.body.style.background = "#008000"
+    }
+if(age >= 20)
+    {
+        document.body.style.background = "#cc66ff"
+    }
+if(age <= 15)
+    {
+        document.body.style.background = "#cc66FF"
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
